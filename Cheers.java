63ee3c79e8ae79adead3518e5cd9e244fc9bb784
Noa <noa.drey@post.runi.ac.il>
2024-11-19 17:14:22 +0200

    1

--- Cheers.java
+++ Cheers.java
@@ -1,6 +1,21 @@
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+         String word = (args[0]);
+         int number = Integer.parseInt(args[1]);
+         word = word.toUpperCase();
+         String vowels = "AEFHILMNORSX";
+        for (int i = 0; i < word.length() ; i++) {
+                if (vowels.indexOf(word.charAt(i)) == -1) {
+                        System.out.println("Give me a  " + word.charAt(i) + ": " + word.charAt(i) + "!");
+                }    
+                else {
+                        System.out.println("Give me an " + word.charAt(i) + ": " + word.charAt(i) + "!"); }         
+                }
+                System.out.println("What does that spell?");
+                for (int j = 0; j < number; j++) {
+                        System.out.println(word + "!!!");
+                }
         }
 }
+
