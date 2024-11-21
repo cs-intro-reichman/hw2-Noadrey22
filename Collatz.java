45fd236df3ad68bbc7b40963fe7af74c5b86a612
Noa <noa.drey@post.runi.ac.il>
2024-11-21 13:51:39 +0200

    4

--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,29 @@
 // Demonstrates the Collatz conjecture.
 public class Collatz {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+		int input = Integer.parseInt(args[0]);
+		String letter = (args[1]);
+		int calc, count = 0;
+		for (int i = 1; i<=input; i++ ){
+			calc = i;
+			count = 1;
+			do { 
+				count++;
+				if (letter.equals("v")) {
+					System.out.print(calc + " ");
+				}
+				if (calc %2 ==0) {
+					calc=calc/2;
+				}else {
+					calc=(calc*3) +1;
+				}
+			}while (calc != 1);
+			if (letter.equals("v")) {
+				System.out.println(1 + " (" + count + ")" ); }
+		}
+		
+	
+		System.out.println("Every one of the first " + input + " hailstone sequences reached 1.");
 	}
+
 }
